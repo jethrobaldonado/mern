08924913fab43874a4090fe1875cd8079f4de441
Jethro Baldonado <jethro.baldonado@gmail.com>
2021-05-25 13:48:01 +0800

    Docker Setup

---
+++ server/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+require("@babel/polyfill");
+
+var _express = _interopRequireDefault(require("express"));
+
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
---
+++ server/src/index.js
@@ -0,0 +1,7 @@
+import '@babel/polyfill';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
